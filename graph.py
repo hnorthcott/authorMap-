9ff16a47ae380db287fd48f40c7f1840ee528a80
Amanda Moulaison <awmoulaison@wpi.edu>
2018-10-03 12:31:50 -0400

    build graphs in matplotlib

--- graph.py
+++ graph.py
@@ -1,10 +1,13 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 import main
 
 
 def makeGraph(d):
     g = nx.Graph(d)
-    nx.write_graphml(g, 'g.xml')
+    graphvis= nx.draw_networkx(g, pos=nx.spring_layout(g), with_labels=True)
+    plt.show(graphvis)
+    #nx.write_graphml(g, 'g.xml')
 
 
 
