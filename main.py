e3c093c3b6f7d7b724702bc1f62390e7ed5646fb
northcotth <htnorthcott@wpi.edu>
2018-10-03 12:57:09 -0400

    seperate utilities into own python file out of main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Bio import Entrez
 import graph
+import utilities
 
 Entrez.email = 'awmoulaison@wpi.edu'
 Entrez.api_key = 'f513b4e2e1a0b578c9d3dd731e36f19f7f08'
@@ -10,51 +11,12 @@ def __init__(self, email):
     Entrez.api_key = 'f513b4e2e1a0b578c9d3dd731e36f19f7f08'
 
 
-def search(query):
-    handle = Entrez.esearch(db='pubmed',
-                            sort='relevance',
-                            retmax='25',
-                            retmode='xml',
-                            term=query)
-    results = Entrez.read(handle)
-    return results
-
-
-def fetch_details(id_list):
-    ids = ','.join(id_list)
-    Entrez.email = 'awmoulaison@wpi.edu'
-    handle = Entrez.efetch(db='pubmed',
-                           retmode='xml',
-                           id=ids)
-    results = Entrez.read(handle)
-    return results
-
-paperDict = {}
-
-
-def summary_details(id_list):
-    ids = ','.join(id_list)
-    Entrez.email = 'awmoulaison@wpi.edu'
-    handle = Entrez.esummary(db = 'pubmed',
-                             retmode = 'xml',
-                             id= ids)
-    records = Entrez.parse(handle)
-
-    for record in records:
-        record['AuthorList'].pop()
-        paperDict[(record['LastAuthor'])] = record['AuthorList']
-        #print(record['AuthorList'])
-        #print(record['LastAuthor'])
-
-    print(paperDict)
-
-
 if __name__ == '__main__':
-    results = search('steroids')
+    results = utilities.search('tuberculosis')
     id_list = results['IdList']
-    papers = fetch_details(id_list)
-    summary_details(id_list)
-    graph.makeGraph(paperDict)
+    papers = utilities.fetch_details(id_list)
+    utilities.summary_details(id_list)
+    graph.makeGraph(utilities.paperDict)
 
     # for i, paper in enumerate(papers['PubmedArticle']):
         # print("%d) %s" % (i + 1, paper['MedlineCitation']['Article']['ArticleTitle']))
