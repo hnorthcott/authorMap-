b4e01e7837c6fb6b62fe5b3c11ce43cd01d290ed
northcotth <htnorthcott@wpi.edu>
2018-10-05 11:45:34 -0400

    connect cytoscape to script- transfer network to cytoscape application

--- graph.py
+++ graph.py
@@ -1,10 +1,26 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 import main
+import json
+from IPython.display import Image
+from py2cytoscape.util import from_networkx
+import requests
+
+server = 'http://localhost:1234/v1'
 
 
 def makeGraph(d):
     g = nx.Graph(d)
+    #graphvis= nx.draw_networkx(g, pos=nx.spring_layout(g), with_labels=True)
+    #plt.show(graphvis)
     nx.write_graphml(g, 'g.xml')
+    authorMap = from_networkx(g)
+    authorNet = requests.post(server + '/networks',
+                            data=json.dumps(authorMap),
+                            headers={'Content-Type': 'application/json'})
+    net_id = authorNet.json()['networkSUID']
+    requests.get('%s/apply/layouts/circular/%d' % (server, net_id))
+    Image('%s/networks/%d/views/first.png' % (server, net_id))
 
 
 
