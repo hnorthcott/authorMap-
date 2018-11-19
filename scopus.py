6b0a53dc6673db6b7712c9b4f7c942aa075124dd
northcotth <htnorthcott@wpi.edu>
2018-11-19 11:21:37 -0500

    implementing influencer algorithm

--- scopus.py
+++ scopus.py
@@ -1,11 +1,16 @@
 # API key = 7dad88ebbea222d693bb348cf0b2cdb3
 from pyscopus import Scopus
+import utilities
 
-key = '7dad88ebbea222d693bb348cf0b2cdb3'
-scopus = Scopus(key)
 
-author_result_df = scopus.search_author("AUTHLAST(Korkin) and AUTHFIRST(Dmitry)")
-print(author_result_df)
+def influencer_detector(d):
+    key = '7dad88ebbea222d693bb348cf0b2cdb3'
+    scopus = Scopus(key)
 
-retrieve_author_info = scopus.retrieve_author('8779137600')
-print(retrieve_author_info)
+    print(d)
+
+    author_result_df = scopus.search_author("AUTHLAST(Korkin) and AUTHFIRST(Dmitry)")
+    print(author_result_df)
+
+    retrieve_author_info = scopus.retrieve_author('8779137600')
+    print(retrieve_author_info)
