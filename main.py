58d777e567d138931d48bcfcc350993d327a899c
northcotth <htnorthcott@wpi.edu>
2018-11-30 13:25:42 -0500

    hello fixing some stuff

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def __init__(self, email):
 
 
 if __name__ == '__main__':
-    results = utilities.search('cancer')
+    results = utilities.search('etiology')
     id_list = results['IdList']
     papers = utilities.fetch_details(id_list)
     utilities.summary_details(id_list)
     utilities.amirs_way(utilities.m_lastAuthor, utilities.m_authorList)
     graph.makeGraph(utilities.amirDict)
-    scopus.influencer_detector(utilities.influencerDict)
+    graph.nodeDegree(graph.g)
     #graph.graphAnalysis(graph.g)
